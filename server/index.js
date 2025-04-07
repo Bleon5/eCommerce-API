2642ae684c42a3dd4da36d4b4b9b394986311cb3
Ali <taspinara17@gmail.com>
2025-04-07 04:24:27 +0200

    orders added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { config } from "dotenv";
 import { Sequelize } from "sequelize";
 import categoryRouter from "./routers/categoryRouter.js";
+import orderRouter from './routers/orderRouter.js';
+import errorHandler from "./middleware/errorHandler.js";
 
 config();
 const PORT = process.env.PORT || 3000;
@@ -12,5 +14,8 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("Hello, from the server!"));
 
 app.use("/category", categoryRouter);
+app.use('/orders', orderRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
